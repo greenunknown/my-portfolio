6ef96960663c6e4802072b742ba088d4db1a15f6
greenunknown <greenunknown0@gmail.com>
2019-02-21 11:24:59 -0800

    Brandon: Added custom text and header to gatsby-config.js and index.js. Changed header color in header.js.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Welcome!</h1>
+    <p>A software engineer's portfolio website built with Gatsby.</p>
+    <h2>About me</h2>
     <p>Now go build something great.</p>
+    <h2>Works</h2>
+    <ul>
+      <li>Setting up a Harbor Docker Registry at Daimler Trucks North America</li>
+    </ul>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
