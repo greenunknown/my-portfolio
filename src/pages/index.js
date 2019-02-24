9492c5d1a772b2b8ba3cbcdd7b3e3d14caa70345
greenunknown <greenunknown0@gmail.com>
2019-02-23 18:02:43 -0800

    Brandon: Adding links to harbor docker registry and adding new content to page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,18 +8,31 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <Link to="/page-2/">Go to page 2</Link>
     <h1>Welcome!</h1>
     <p>A software engineer's portfolio website built with Gatsby.</p>
     <h2>About me</h2>
     <p>Now go build something great.</p>
-    <h2>Works</h2>
+    <h2>Selected Works</h2>
+    
     <ul>
-      <li>Setting up a Harbor Docker Registry at Daimler Trucks North America</li>
+      <li>
+        <p>Setting up a Harbor Docker Registry at Daimler Trucks North America</p>
+        <img src="https://raw.githubusercontent.com/goharbor/harbor/master/docs/img/harbor_logo.png" alt="harbor_logo.png"></img>
+        <img src="https://raw.githubusercontent.com/goharbor/harbor/master/docs/img/rbac.png" alt="rbac.png"></img>
+        <img src="https://raw.githubusercontent.com/goharbor/harbor/master/docs/img/new_project_log.png" alt="new_project_log.png"></img>
+
+      </li>
+      <li>
+        LScan CS Capstone Project: 
+      </li>
+      <li>
+        CS 447: Computer Graphics Amusement Park Project
+      </li>
     </ul>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
